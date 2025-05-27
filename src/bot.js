0b3944c99bcb6649e138b88e4610bc2097893ae6
jaka8m <paoandest82@gmail.com>
2025-05-28 07:13:16 +0800

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,6 @@
-import { randomip, getIpDetail, getFlagEmoji } from './randomip.js';
+import { handleCommand } from './commandHandler.js';
+import { handleCallback, answerCallback, editMessageReplyMarkup } from './callbackHandler.js';
+import { randomip } from './randomip.js';
 
 export default class TelegramBot {
   constructor(token, apiUrl = 'https://api.telegram.org') {
@@ -15,38 +17,17 @@ export default class TelegramBot {
       const userId = message.from.id;
       const text = message.text || '';
 
-      if (text.startsWith('/randomip')) {
-        await this.sendMessage(chatId, '⏳ Mengambil IP proxy acak...');
-        const { text: resultText, buttons } = await randomip(userId, 1);
-        await this.sendMessage(chatId, resultText, {
-          parse_mode: 'Markdown',
-          reply_markup: { inline_keyboard: buttons }
-        });
-      }
+      await handleCommand({ text, chatId, userId, sendMessage: this.sendMessage.bind(this) });
 
     } else if (callback) {
-      const { data, message, from, id: callbackId } = callback;
-
-      if (data.startsWith('DETAIL_')) {
-        const code = data.split('_')[1];
-        const detailList = getIpDetail(from.id, code);
-
-        if (!detailList) {
-          await this.answerCallback(callbackId, 'Data tidak ditemukan.');
-          return;
-        }
-
-        const detailText = `*Detail IP dari ${code} ${getFlagEmoji(code)}:*\n\n${detailList.join('\n\n')}`;
-        await this.sendMessage(message.chat.id, detailText, { parse_mode: 'Markdown' });
-        await this.answerCallback(callbackId);
-      } else if (data.startsWith('PAGE_')) {
-        const page = parseInt(data.split('_')[1], 10);
-        const { text, buttons } = await randomip(from.id, page);
-
-        // Edit pesan inline keyboard (paging)
-        await this.editMessageReplyMarkup(message.chat.id, message.message_id, buttons);
-        await this.answerCallback(callbackId);
-      }
+      await handleCallback({
+        callback,
+        sendMessage: this.sendMessage.bind(this),
+        answerCallback: answerCallback.bind(this),
+        editMessageReplyMarkup: editMessageReplyMarkup.bind(this),
+        token: this.token,
+        apiUrl: this.apiUrl
+      });
     }
 
     return new Response('OK', { status: 200 });
@@ -58,34 +39,6 @@ export default class TelegramBot {
       chat_id: chatId,
       text,
       ...options
-    };
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(payload)
-    });
-    return response.json();
-  }
-
-  async answerCallback(callbackId, text = '') {
-    const url = `${this.apiUrl}/bot${this.token}/answerCallbackQuery`;
-    await fetch(url, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        callback_query_id: callbackId,
-        text,
-        show_alert: false
-      })
-    });
-  }
-
-  async editMessageReplyMarkup(chatId, messageId, inlineKeyboard) {
-    const url = `${this.apiUrl}/bot${this.token}/editMessageReplyMarkup`;
-    const payload = {
-      chat_id: chatId,
-      message_id: messageId,
-      reply_markup: { inline_keyboard: inlineKeyboard }
     };
     const response = await fetch(url, {
       method: 'POST',
