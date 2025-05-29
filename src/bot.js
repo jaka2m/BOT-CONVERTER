ee5a31baa16c32625e821c6f43604c209cdb1a74
jaka8m <paoandest82@gmail.com>
2025-05-29 17:29:51 +0800

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -3,6 +3,7 @@ import { checkProxyIP } from './checkip.js';
 import { randomconfig } from './randomconfig.js';
 import { rotateconfig } from './config.js';
 import TelegramBotku from './randomip/bot2.js';
+import { botku } from './randomip/bot2.js';
 
 const HOSTKU = 'example.com';
 
