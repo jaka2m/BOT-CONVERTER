4a9d5f776fb8f59a588939cddbf326335ff27b3f
jaka8m <paoandest82@gmail.com>
2025-05-29 13:55:28 +0800

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -2,11 +2,9 @@ import { generateClashConfig, generateNekoboxConfig, generateSingboxConfig } fro
 import { checkProxyIP } from './checkip.js';
 import { randomconfig } from './randomconfig.js';
 import { rotateconfig } from './config.js';
-import { handleCommand as handleCommandFromCommandHandler } from './randomip/commandHandler.js';
-
-import { handleCommand as handleCommandFromTom } from './randomip/tom.js';
-
+import { handleCommand } from './randomip/commandHandler.js';
 import { handleCallback, answerCallback, editMessageReplyMarkup } from './randomip/callbackHandler.js';
+import { randomip } from './randomip/tom.js';
 
 const HOSTKU = 'example.com';
 
