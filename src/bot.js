8219a5ff5bc5da78ffc5da7f33213b0c079b614e
jaka8m <paoandest82@gmail.com>
2025-05-29 15:20:23 +0800

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -2,9 +2,6 @@ import { generateClashConfig, generateNekoboxConfig, generateSingboxConfig } fro
 import { checkProxyIP } from './checkip.js';
 import { randomconfig } from './randomconfig.js';
 import { rotateconfig } from './config.js';
-import { handleCommand } from './randomip/commandHandler.js';
-import { handleCallback, answerCallback, editMessageReplyMarkup } from './randomip/callbackHandler.js';
-import { randomip } from './randomip/randomip.js';
 
 const HOSTKU = 'example.com';
 
@@ -87,36 +84,6 @@ export default class TelegramBot {
         return new Response('OK', { status: 200 });
       }
       
-      if (text.startsWith('/randomip')) {
-  try {
-    const response = await fetch('https://raw.githubusercontent.com/jaka2m/botak/refs/heads/main/cek/proxyList.txt');
-    const ipText = await response.text();
-    const ipList = ipText
-      .split('\n')
-      .map(line => line.trim())
-      .filter(line => line !== '');
-
-    if (ipList.length === 0) {
-      await this.sendMessage(chatId, `⚠️ *Daftar IP kosong atau tidak ditemukan. Coba lagi nanti.*`, { parse_mode: 'Markdown' });
-      return new Response('OK', { status: 200 });
-    }
-
-    // Ambil maksimal 20 IP secara acak
-    const shuffled = ipList.sort(() => 0.5 - Math.random());
-    const selectedIPs = shuffled.slice(0, 20);
-
-    let resultText = `🔑 *Here are ${selectedIPs.length} random Proxy IPs:*\n\n`;
-    selectedIPs.forEach(line => {
-      const [ip, port, code, isp] = line.split(',');
-      resultText += `📍 *IP:PORT* : \`${ip}:${port}\`\n`;
-      resultText += `🌐 *Country* : ${code} ${getFlagEmoji(code)}\n`;
-      resultText += `💻 *ISP* : ${isp}\n\n`;
-    });
-
-    await this.sendMessage(chatId, resultText, { parse_mode: 'Markdown' });
-    return new Response('OK', { status: 200 });
-  }
-  
       // /config command
       if (text.startsWith('/config')) {
         const helpMsg = `🌟 *PANDUAN CONFIG ROTATE* 🌟
