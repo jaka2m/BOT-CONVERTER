d73b9eede83324a94191d8eed6432a668f34653e
jaka8m <paoandest82@gmail.com>
2025-05-29 17:02:53 +0800

    Create bot2.js

---
+++ src/randomip/bot2.js
@@ -0,0 +1,74 @@
+import {
+  handleRandomIpCommand,
+  handleCallbackQuery,
+} from './randomip/randomip.js';
+
+export default class TelegramBot {
+  constructor(token, apiUrl = 'https://api.telegram.org') {
+    this.token = token;
+    this.apiUrl = apiUrl;
+  }
+
+  async handleUpdate(update) {
+    if (update.callback_query) {
+      await handleCallbackQuery(this, update.callback_query);
+      return new Response('OK', { status: 200 });
+    }
+
+    if (!update.message) return new Response('OK', { status: 200 });
+
+    const chatId = update.message.chat.id;
+    const text = update.message.text || '';
+
+    if (text.startsWith('/randomip')) {
+      await handleRandomIpCommand(this, chatId);
+    } else {
+      await this.sendMessage(chatId, 'Gunakan perintah /randomip untuk mendapatkan IP acak.');
+    }
+
+    return new Response('OK', { status: 200 });
+  }
+
+  async sendMessage(chatId, text, options = {}) {
+    const url = `${this.apiUrl}/bot${this.token}/sendMessage`;
+    const body = {
+      chat_id: chatId,
+      text,
+      ...options,
+    };
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    });
+
+    return response.json();
+  }
+
+  async editMessageReplyMarkup({ chat_id, message_id, reply_markup }) {
+    const url = `${this.apiUrl}/bot${this.token}/editMessageReplyMarkup`;
+    const body = { chat_id, message_id, reply_markup };
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    });
+
+    return response.json();
+  }
+
+  async answerCallbackQuery(callbackQueryId) {
+    const url = `${this.apiUrl}/bot${this.token}/answerCallbackQuery`;
+    const body = { callback_query_id: callbackQueryId };
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    });
+
+    return response.json();
+  }
+}
