ccb58c74e3116370aaaecd4700ebab36ebb433c6
jaka8m <paoandest82@gmail.com>
2025-05-29 09:02:14 +0800

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -30,7 +30,6 @@ export default class TelegramBot {
       const [action, ipPort] = data.split('|');
       
       if (message) {
-      const userId = message.from.id;
       const text = message.text || '';
 
 
