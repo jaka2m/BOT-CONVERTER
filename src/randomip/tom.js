d2dd25dad0078317596c37cb19f16a40aff6085e
jaka8m <paoandest82@gmail.com>
2025-05-29 13:26:19 +0800

    Create tom.js

---
+++ src/randomip/tom.js
@@ -0,0 +1,29 @@
+export async function jamur(update) {
+  if (update.callback_query) {
+    const callback = update.callback_query;
+    const chatId = callback.message.chat.id;
+    const messageId = callback.message.message_id;
+
+    await handleCallback({
+      callback,
+      sendMessage: this.sendMessage.bind(this),
+      answerCallback: this.answerCallback.bind(this),
+      editMessageReplyMarkup: this.editMessageReplyMarkup.bind(this),
+      token: this.token,
+      apiUrl: this.apiUrl
+    });
+  } else if (update.message) {
+    const text = update.message.text || '';
+    const chatId = update.message.chat.id;
+    const userId = update.message.from.id;
+
+    await handleCommand({
+      text,
+      chatId,
+      userId,
+      sendMessage: this.sendMessage.bind(this)
+    });
+  }
+
+  return new Response('OK', { status: 200 });
+}
