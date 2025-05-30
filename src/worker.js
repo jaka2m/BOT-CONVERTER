12e81a9995f7f7e2f453efc35be3ac33101dcd61
jaka8m <paoandest82@gmail.com>
2025-05-30 20:17:40 +0800

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -1,5 +1,5 @@
 import { TelegramBot as Bot1 } from './bot.js';
-import { TelegramBotku as Bot3 } from './proxyip/bot2.js';
+import { TelegramBotku as Bot3 } from './randomip/bot2.js';
 import { TelegramProxyCekBot as Bot4 } from './proxyip/botCek.js';
 
 export default {
