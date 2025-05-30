1d4904919ea77e99d52b47869a395ddfdc9553be
jaka8m <paoandest82@gmail.com>
2025-05-30 15:04:28 +0800

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -1,7 +1,6 @@
 import { TelegramBot as Bot1 } from './bot.js';
 import { TelegramBotku as Bot2 } from './randomip/bot2.js';
 import { TelegramProxyBot as Bot3 } from './proxyip/bot3.js';
-import { ConverterBotku as Bot4 } from './converter/converter.js';
 
 export default {
   async fetch(request, env) {
@@ -15,15 +14,13 @@ export default {
       const bot1 = new Bot1(env.TELEGRAM_BOT_TOKEN);
       const bot2 = new Bot2(env.TELEGRAM_BOT_TOKEN);
       const bot3 = new Bot3(env.TELEGRAM_BOT_TOKEN);
-      const bot4 = new Bot4(env.TELEGRAM_BOT_TOKEN);
       
       // Jalankan handleUpdate di kedua bot secara berurutan
       // Biasanya cuma butuh return salah satu response (misalnya dari bot1)
       await bot1.handleUpdate(update);
       await bot2.handleUpdate(update);
       await bot3.handleUpdate(update);
-      await bot4.handleUpdate(update);
-
+      
       // Kembalikan response 200 OK
       return new Response('OK', { status: 200 });
     } catch (error) {
