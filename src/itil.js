9912f234d1816ea9873825862ec1c131e7374b31
jaka8m <paoandest82@gmail.com>
2025-06-01 14:00:50 +0800

    Update itil.js

--- src/itil.js
+++ src/itil.js
@@ -1,13 +1,12 @@
-// === KONFIG TELEGRAM & CLOUDFLARE ===
 export const TELEGRAM_BOT_TOKEN = "7791564952:AAHXcZF0NFk512tuUNs7iEzJ12DZpLyOpo4";
-export const CLOUDFLARE_CONFIG = {
-  apiKey: "5fae9fcb9c193ce65de4b57689a94938b708e",
-  accountID: "e9930d5ca683b0461f73477050fee0c7",
-  zoneID: "80423e7547d2fa85e13796a1f41deced",
-  email: "ambebalong@gmail.com",
-  serviceName: "siren",
-  rootDomain: "joss.checker-ip.xyz"
-};
+export const apiKey = "5fae9fcb9c193ce65de4b57689a94938b708e";
+export const accountID = "e9930d5ca683b0461f73477050fee0c7";
+export const zoneID = "80423e7547d2fa85e13796a1f41deced";
+export const apiEmail = "ambebalong@gmail.com";
+export const serviceName = "siren";
+export const rootDomain = "joss.checker-ip.xyz";
+
+// GANTI dengan ID pemilik (boleh lebih dari satu)
 export const OWNER_IDS = [1467883032, 987654321];
 
 // === WILDCARD FILE HANDLING ===
