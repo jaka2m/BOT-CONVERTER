2faf9ed29e2750dd9d1c66f2292e557e4527776a
jaka8m <paoandest82@gmail.com>
2025-06-01 13:57:25 +0800

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -1,9 +1,13 @@
 import TelegramBot from './bot.js';
 import { TELEGRAM_BOT_TOKEN } from './itil.js';
 
-const bot = new TelegramBot(TELEGRAM_BOT_TOKEN);
+async function main() {
+  const bot = new TelegramBot(TELEGRAM_BOT_TOKEN);
 
-const rawBody = await Bun.stdin.text(); // Kalau pakai Bun
-const update = JSON.parse(rawBody);
+  const rawBody = await Bun.stdin.text(); // kalau pakai Bun
+  const update = JSON.parse(rawBody);
 
-await bot.handleUpdate(update);
+  await bot.handleUpdate(update);
+}
+
+main().catch(console.error);
