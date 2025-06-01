7d0c22d2a1c95d1305bbf5ebf9596c956e73ee66
jaka8m <paoandest82@gmail.com>
2025-06-01 14:22:01 +0800

    Update itil.js

--- src/itil.js
+++ src/itil.js
@@ -1,4 +1,7 @@
-export const TELEGRAM_BOT_TOKEN = "7664381872:AAHfoMYhaUYlzRgMlydfA3zvwkMyVLMhoTU";
+// itil.js
+
+// Data Cloudflare
+export const TELEGRAM_BOT_TOKEN = "7791564952:AAHXcZF0NFk512tuUNs7iEzJ12DZpLyOpo4";
 export const apiKey = "5fae9fcb9c193ce65de4b57689a94938b708e";
 export const accountID = "e9930d5ca683b0461f73477050fee0c7";
 export const zoneID = "80423e7547d2fa85e13796a1f41deced";
@@ -9,22 +12,17 @@ export const rootDomain = "joss.checker-ip.xyz";
 // GANTI dengan ID pemilik (boleh lebih dari satu)
 export const OWNER_IDS = [1467883032, 987654321];
 
-// === WILDCARD FILE HANDLING ===
-const FILE = './wildcards.txt';
-
 export function isAdmin(userId) {
   return OWNER_IDS.includes(userId);
 }
 
+// Wildcards disimpan di memori sementara (non-persistent)
+let wildcardsList = [];
+
 export async function readWildcards() {
-  try {
-    const data = await fs.readFile(FILE, 'utf8');
-    return data.split('\n').filter(Boolean);
-  } catch {
-    return [];
-  }
+  return wildcardsList;
 }
 
 export async function writeWildcards(list) {
-  await fs.writeFile(FILE, list.join('\n'), 'utf8');
+  wildcardsList = list;
 }
