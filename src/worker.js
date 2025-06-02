d30aeadd3b45f6ce2a10cef9f0a766ce9a09192d
jaka8m <paoandest82@gmail.com>
2025-06-02 20:34:11 +0800

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -2,7 +2,7 @@ import { TelegramBot as Bot1 } from './bot.js';
 import { TelegramBotku as Bot2 } from './randomip/bot2.js';
 import { TelegramProxyCekBot as Bot3 } from './proxyip/botCek.js';
 import { TelegramProxyBot as Bot4 } from './proxyip/bot3.js';
-import { TelegramWildcardBot, CloudflareBot } from './wildcard/botwild.js';
+import { TelegramWildcardBot as Bot5 } from './wildcard/botwild.js';
 
 export default {
   async fetch(request, env) {
@@ -12,25 +12,23 @@ export default {
 
     try {
       const update = await request.json();
+
       const token = env.TELEGRAM_BOT_TOKEN;
       const ownerId = Number(env.OWNER_ID);
 
-      // Buat instance CloudflareBot (untuk digunakan oleh TelegramWildcardBot)
-      const cfBot = new CloudflareBot({
-        rootDomain: env.ROOT_DOMAIN || 'joss.checker-ip.xyz', // sesuaikan jika perlu
-        apiKey: env.API_KEY,
-        apiEmail: env.API_EMAIL,
-        accountID: env.ACCOUNT_ID,
-        zoneID: env.ZONE_ID,
-        serviceName: env.SERVICE_NAME || 'siren',
-      });
-
-      // Inisialisasi semua bot
       const bot1 = new Bot1(token, 'https://api.telegram.org', ownerId);
       const bot2 = new Bot2(token, 'https://api.telegram.org', ownerId);
       const bot3 = new Bot3(token, 'https://api.telegram.org', ownerId);
       const bot4 = new Bot4(token, 'https://api.telegram.org', ownerId);
-      const bot5 = new TelegramWildcardBot(token, 'https://api.telegram.org', ownerId, cfBot); // ← cfBot instance dikirim ke sini
+      const bot5 = new Bot5(token, ownerId, {
+        apiUrl: 'https://api.telegram.org',
+        accountID: env.ACCOUNT_ID,
+        zoneID: env.ZONE_ID,
+        apiKey: env.API_KEY,
+        apiEmail: env.API_EMAIL,
+        serviceName: env.SERVICE_NAME || 'siren',
+        rootDomain: env.ROOT_DOMAIN || '',
+      });
 
       await Promise.all([
         bot1.handleUpdate(update),
