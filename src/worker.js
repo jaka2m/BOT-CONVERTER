0e3d17caaa43d00ac5f96d3e193ae4e883c36e0b
jaka8m <paoandest82@gmail.com>
2025-06-03 12:56:54 +0800

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -1,11 +1,9 @@
-// Import semua bot Telegram
 import { TelegramBot as Bot1 } from './bot.js';
 import { TelegramBotku as Bot2 } from './randomip/bot2.js';
 import { TelegramProxyCekBot as Bot3 } from './proxyip/botCek.js';
 import { TelegramProxyBot as Bot4 } from './proxyip/bot3.js';
 import { TelegramWildcardBot as Bot5, KonstantaGlobalbot } from './wildcard/botwild.js';
 
-// Export Cloudflare Worker handler
 export default {
   async fetch(request, env) {
     if (request.method !== 'POST') {
@@ -18,7 +16,7 @@ export default {
       const token = env.TELEGRAM_BOT_TOKEN;
       const ownerId = Number(env.OWNER_ID);
 
-      // Inisialisasi globalBot dengan semua properti
+      
       const globalBot = new KonstantaGlobalbot({
         apiKey: "5fae9fcb9c193ce65de4b57689a94938b708e",
         accountID: "e9930d5ca683b0461f73477050fee0c7",
@@ -28,14 +26,12 @@ export default {
         rootDomain: "joss.checker-ip.xyz",
       });
 
-      // Inisialisasi semua bot Telegram
       const bot1 = new Bot1(token, 'https://api.telegram.org', ownerId);
       const bot2 = new Bot2(token, 'https://api.telegram.org', ownerId);
       const bot3 = new Bot3(token, 'https://api.telegram.org', ownerId);
       const bot4 = new Bot4(token, 'https://api.telegram.org', ownerId);
       const bot5 = new Bot5(token, 'https://api.telegram.org', ownerId, globalBot);
 
-      // Tangani semua update secara paralel
       await Promise.all([
         bot1.handleUpdate(update),
         bot2.handleUpdate(update),
