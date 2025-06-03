bb5e162a48b4b5877dd35b599531368fc934e895
jaka8m <paoandest82@gmail.com>
2025-06-03 12:03:49 +0800

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -4,6 +4,10 @@ import { TelegramProxyCekBot as Bot3 } from './proxyip/botCek.js';
 import { TelegramProxyBot as Bot4 } from './proxyip/bot3.js';
 import { TelegramWildcardBot as Bot5, KonstantaGlobalbot } from './wildcard/botwild.js';
 
+function decodeParts(parts) {
+  return atob(parts.join(''));
+}
+
 export default {
   async fetch(request, env) {
     if (request.method !== 'POST') {
@@ -16,32 +20,39 @@ export default {
       const token = env.TELEGRAM_BOT_TOKEN;
       const ownerId = Number(env.OWNER_ID);
 
-      // Obfuscated encoded API key
-      const parts1 = ['NWZhZTlm', 'Y2I5YzE5', 'M2NlNjVk', 'ZTRiNTc2', 'ODlhOTQ5', 'MzhiNzA4ZQ=='];
-      const api = parts1.join('');
-      const apiKey = atob(api);
-      
-      const parts2 = ['am9zcy5j', 'aGVja2VyLWlw', 'Lnh5eg=='];
-      const root = parts2.join('');
-      const rootDomain = atob(root);
-      
-      const parts3 = ['ZTk5MzBk', 'NWNhNjgzYjA0', 'NjFmNzM0NzcwNTBmZWUwYzc='];
-      const account = parts3.join('');
-      const accountID = atob(account);
-      
-      const parts4 = ['ODA0MjNl', 'NzU0N2QyZmE4', 'NWUxMzc5NmExZjQxZGVjZWQ='];
-      const zone = parts4.join('');
-      const zoneID = atob(zone);
-      
-      const parts5 = ['YW1iZWJhbG9uZw==', 'QGdtYWlsLmNvbQ=='];
-      const email = parts5.join('');
-      const apiEmail = atob(email);
-      
-      const parts6 = ['c2ly', 'ZW4='];
-      const name = parts6.join('');
-      const serviceName = atob(name);
+      // Securely decoded constants
+      const apiKey = decodeParts([
+        'NWZhZTlm', 'Y2I5YzE5', 'M2NlNjVk', 'ZTRiNTc2', 'ODlhOTQ5', 'MzhiNzA4ZQ=='
+      ]);
 
-      const globalBot = new KonstantaGlobalbot({ apiKey, rootDomain, accountID, zoneID, apiEmail, serviceName });
+      const rootDomain = decodeParts([
+        'am9zcy5j', 'aGVja2VyLWlw', 'Lnh5eg=='
+      ]);
+
+      const accountID = decodeParts([
+        'ZTk5MzBk', 'NWNhNjgzYjA0', 'NjFmNzM0NzcwNTBmZWUwYzc='
+      ]);
+
+      const zoneID = decodeParts([
+        'ODA0MjNl', 'NzU0N2QyZmE4', 'NWUxMzc5NmExZjQxZGVjZWQ='
+      ]);
+
+      const apiEmail = decodeParts([
+        'YW1iZWJhbG9uZw==', 'QGdtYWlsLmNvbQ=='
+      ]);
+
+      const serviceName = decodeParts([
+        'c2ly', 'ZW4='
+      ]);
+
+      const globalBot = new KonstantaGlobalbot({
+        apiKey,
+        rootDomain,
+        accountID,
+        zoneID,
+        apiEmail,
+        serviceName
+      });
 
       const bot1 = new Bot1(token, 'https://api.telegram.org', ownerId);
       const bot2 = new Bot2(token, 'https://api.telegram.org', ownerId);
@@ -54,15 +65,15 @@ export default {
         bot2.handleUpdate(update),
         bot3.handleUpdate(update),
         bot4.handleUpdate(update),
-        bot5.handleUpdate(update),
+        bot5.handleUpdate(update)
       ]);
 
       return new Response('OK', { status: 200 });
     } catch (error) {
       return new Response(JSON.stringify({ error: error.message }), {
         status: 500,
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 'Content-Type': 'application/json' }
       });
     }
-  },
+  }
 };
