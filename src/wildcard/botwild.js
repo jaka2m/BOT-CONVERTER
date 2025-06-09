08d1cf0c258226097aec3022f8f5f045a1ae7a3c
jaka8m <paoandest82@gmail.com>
2025-06-09 10:33:11 +0800

    Update botwild.js

--- src/wildcard/botwild.js
+++ src/wildcard/botwild.js
@@ -315,7 +315,7 @@ support.zoom.us
           `${i+1}\\. ${this.escapeMarkdownV2(d)}`
         ).join('\n');
         await this.sendMessage(chatId,
-          ````🌐 List Custom Domains :\n${listText}```\n\n📊 Total: *${domains.length}* subdomain${domains.length>1?'s':''}`,
+          `🌐 List Custom Domains :\n\`${listText}\`\n\n📊 Total: *${domains.length}* subdomain${domains.length>1?'s':''}`,
           { parse_mode: 'MarkdownV2' }
         );
         const fileContent = domains.map((d,i)=>`${i+1}. ${d}`).join('\n');
